760b0bbcf385bc5996af475abb28cb90912100a7
Sarvex ☠ Jatasra <sarvex.jatasra@gmail.com>
2024-08-19 10:49:23 +0530

    Added Github Workflows

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,4 @@
+Get-ChildItem -Filter *.md -Recurse | Remove-Item
+
 Get-ChildItem -Filter *.java -Recurse | Rename-Item -NewName { $_.Directory.Name+'.java'}
 Get-ChildItem -Filter *.java -Recurse | Move-Item -Destination { $_.Directory.Parent.FullName }
-Get-ChildItem -Filter *.md -Recurse | Remove-Item
